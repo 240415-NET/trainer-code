f6a109259bb390ce4833013a456e1426beb00a35
Brian Arayathel <brian.arayathel@revature.com>
2024-04-24 12:51:33 -0400

    added todo list basic example

--- Week2/ClassBasics/Program.cs
+++ Week2/ClassBasics/Program.cs
@@ -5,78 +5,76 @@ namespace ClassBasics
 {
     public class ClassBasics
     {
-        public static void Main(string[] args)
-        {
-            ExampleClass exampleClass; // Declaration
+        // public static void Main(string[] args)
+        // {
+        //     ExampleClass exampleClass; // Declaration
 
-            exampleClass = new ExampleClass(); // Setting through instantiation
+        //     exampleClass = new ExampleClass(); // Setting through instantiation
 
-            exampleClass.Age = 55; // Updating
+        //     exampleClass.Age = 55; // Updating
 
-            // Initialization is all of it in one fell swoop
-
-
-            // exampleClass.Name = "    Hello   "; // this will not compile as we are accessing a private variable
-
-            exampleClass.SetName("   Hello         ");
-            // Console.WriteLine(exampleClass.GetName());
-
-            exampleClass.Age = 55; // this is fine because the Age field is public
-
-            // Console.WriteLine(exampleClass.GetMaxLimit());
+        //     // Initialization is all of it in one fell swoop
 
 
+        //     // exampleClass.Name = "    Hello   "; // this will not compile as we are accessing a private variable
 
+        //     exampleClass.SetName("   Hello         ");
+        //     // Console.WriteLine(exampleClass.GetName());
 
-            // Person Class Examples
+        //     exampleClass.Age = 55; // this is fine because the Age field is public
 
-            Person personA = new Person();
-
-            // Console.WriteLine(personA.GetFirstName());
-            // Console.WriteLine(personA.GetLastName());
-            // Console.WriteLine(personA.GetEmail());
-            // Console.WriteLine(personA.GetAge());
-            // Console.WriteLine(personA.GetOnHoliday());
-
-
-            Person personB = new Person(); // invoking the default constructor
-            personB.SetFirstName(""); // will default to John because of the conditional in the setter method
-            personB.SetLastName("Arayathel");
-            personB.SetEmail("brian.arayathel@revature.com");
-            personB.SetAge(-2); // will default to 18 because of the conditional in the setter method
-            personB.SetOnHoliday(false);
-
-            // Console.WriteLine(personB.GetFirstName());
-            // Console.WriteLine(personB.GetLastName());
-            // Console.WriteLine(personB.GetEmail());
-            // Console.WriteLine(personB.GetAge());
-            // Console.WriteLine(personB.GetOnHoliday());
+        //     // Console.WriteLine(exampleClass.GetMaxLimit());
 
 
 
-            Person personC = new Person("Jane", "Smith", "jane.smith@email.com", 33, true); // invoking the parameterized constructor
-            // Console.WriteLine(personC);
 
-            // Console.WriteLine(personC.GetFirstName());
-            // Console.WriteLine(personC.GetLastName());
-            // Console.WriteLine(personC.GetEmail());
-            // Console.WriteLine(personC.GetAge());
-            // Console.WriteLine(personC.GetOnHoliday());
+        //     // Person Class Examples
 
-            List<Person> people = new List<Person>();
+        //     Person personA = new Person();
 
-            people.Add(personA);
-            people.Add(personB);
-            people.Add(personC);
-
-            foreach(var person in people)
-            {
-                Console.WriteLine(person);
-            }
+        //     // Console.WriteLine(personA.GetFirstName());
+        //     // Console.WriteLine(personA.GetLastName());
+        //     // Console.WriteLine(personA.GetEmail());
+        //     // Console.WriteLine(personA.GetAge());
+        //     // Console.WriteLine(personA.GetOnHoliday());
 
 
+        //     Person personB = new Person(); // invoking the default constructor
+        //     personB.SetFirstName(""); // will default to John because of the conditional in the setter method
+        //     personB.SetLastName("Arayathel");
+        //     personB.SetEmail("brian.arayathel@revature.com");
+        //     personB.SetAge(-2); // will default to 18 because of the conditional in the setter method
+        //     personB.SetOnHoliday(false);
 
-        }
+        //     // Console.WriteLine(personB.GetFirstName());
+        //     // Console.WriteLine(personB.GetLastName());
+        //     // Console.WriteLine(personB.GetEmail());
+        //     // Console.WriteLine(personB.GetAge());
+        //     // Console.WriteLine(personB.GetOnHoliday());
+
+
+
+        //     Person personC = new Person("Jane", "Smith", "jane.smith@email.com", 33, true); // invoking the parameterized constructor
+        //     // Console.WriteLine(personC);
+
+        //     // Console.WriteLine(personC.GetFirstName());
+        //     // Console.WriteLine(personC.GetLastName());
+        //     // Console.WriteLine(personC.GetEmail());
+        //     // Console.WriteLine(personC.GetAge());
+        //     // Console.WriteLine(personC.GetOnHoliday());
+
+        //     List<Person> people = new List<Person>();
+
+        //     people.Add(personA);
+        //     people.Add(personB);
+        //     people.Add(personC);
+
+        //     foreach(var person in people)
+        //     {
+        //         Console.WriteLine(person);
+        //     }
+
+        // }
     }
 
 }
---
+++ Week2/ClassBasics/TodoItem.cs
@@ -0,0 +1,123 @@
+using System.Runtime.InteropServices;
+
+namespace ClassBasics.TodoList
+{
+    public class TodoItem
+    {
+
+        // These are the fields that will make up the shape of my object
+        private string Description = "Default Description";
+        private int EstimatedTime = 60; // int as minutes
+
+        private string DueDate = "4/24/2024";
+
+        private bool Status = false; // false is incomplete
+
+        public TodoItem(){
+
+        }
+
+        public TodoItem(string Description, int EstimatedTime, string DueDate, bool Status)
+        {
+            this.Description = Description;
+            this.EstimatedTime = EstimatedTime;
+            this.DueDate = DueDate;
+            this.Status = Status;
+        }
+
+        // My methods are how I will interact with the objects, and how the objects will interact with each other
+        public string GetDescription(){
+            return this.Description;
+        }
+
+        public void SetDescription(string Description){
+            this.Description = Description;
+        }
+
+        public bool GetStatus(){
+            return this.Status;
+        }
+
+        public void SetStatus(bool Status){
+            this.Status = Status;
+        }
+
+        public int GetEstimatedTime(){
+            return this.EstimatedTime;
+        }
+
+        public void SetEstimatedTime(int EstimatedTime){
+            this.EstimatedTime = EstimatedTime;
+        }
+
+        public string GetDueDate(){
+            return this.DueDate;
+        }
+
+        public void SetDueDate(string DueDate){
+            this.DueDate = DueDate;
+        }
+
+
+        // Override the ToString
+
+        public override string ToString()
+        {
+            // I do not want to put a raw boolean inside my ToString, so I have created my own string representing the status of the boolean
+            // It will default to false, showing incomplete
+            string CurrentStatus = "Incomplete";
+
+
+            // If the todo Item field Status is set to be true, then this If statement will evaluate
+            // And update the CurrentStatus string to "Complete"
+            if(Status){
+                CurrentStatus = "Complete";
+            }
+
+            return $"{Description} - {DueDate}\nEstimated Time: {EstimatedTime}\nStatus: {CurrentStatus}";
+        }
+
+        public static void Main(string[] args){
+            // TodoItem item1 = new TodoItem();
+
+            // Console.WriteLine(item1);
+
+            // TodoItem item2 = new TodoItem("Get Milk", 60, "4/25/2024", false);
+
+            // Console.WriteLine(item2);
+
+
+            List<TodoItem> todoItems = new List<TodoItem>();
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("Type Description:\n");
+
+                string description = Console.ReadLine();
+
+                Console.WriteLine("Estimated Time:\n");
+
+                int estimatedTime = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Due Date:\n");
+
+                string dueDate = Console.ReadLine();
+
+                TodoItem newItem = new TodoItem(description, estimatedTime, dueDate, false);
+
+                todoItems.Add(newItem);
+            }
+
+
+            foreach(var item in todoItems){
+                Console.WriteLine(item);
+            }
+
+            // Organizing my UI into a class
+            // Adding validation and checks for userinput and saving to my object
+            // Adding a way to add items individually, instead of 6 at a time
+            // etc
+
+        }
+    }
+}
