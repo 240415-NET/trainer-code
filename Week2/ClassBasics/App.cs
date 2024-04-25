97168f5b8050228d682c886b700144bd6a4e42df
Brian Arayathel <brian.arayathel@revature.com>
2024-04-25 12:00:13 -0400

    mid training commit

--- Week2/ClassBasics/App.cs
+++ Week2/ClassBasics/App.cs
@@ -134,11 +134,11 @@ namespace ClassBasics
         {
             /**
                 Polymorphism
-
-                    - Polymorphism allows emthods to do different things based on the object that is acting on them
+                    - Polymorphism allows methods to do different things based on the object that is acting on them
                     - This can be achieved by method overriding
                         - Where a child class can provide their own specific implementation of a method already provided by one of its parent classes
                         - For example, overriding the ToString() method
+                        - In order to override a method in C#, the method in the parent class must be defined with the keyword virtual and in the child class it must use the keyword override
                     - This can be achieved by method overloading
                         - Where multiple methods can have the same name but different parameters or order of parameters
                             - Unique method signatures
@@ -150,6 +150,27 @@ namespace ClassBasics
             Console.WriteLine(polymorphismExample.Add(1, 2));
             Console.WriteLine(polymorphismExample.Add(1.5f, 2.23432f));
             Console.WriteLine(polymorphismExample.Add(long.MaxValue / 2, long.MinValue / 5));
+            Console.WriteLine(polymorphismExample);
+
+            /*
+                Inheritance
+
+                    - Inheritance allows a class to inherit attributes and methods from another class
+                    - The class which inehrits is called the subclass/ derived class/ child class
+                    - The class that is being inherited is called the superclass/ base class/ parent class
+                    - Inheritance enforces heirarchical structures to your objects and classes
+                    - Class A -> Class B -> Class C -> Class D
+                        - Class D will have everything above it
+                        - Class A will not have anything from below it
+            */
+
+            Animal animal = new Animal("Monkey", "Ooh Ooh Ahh Ahh");
+            Dog lassie = new Dog(42, true);
+            Dog pancake = new Dog("Pancake", 30, true, "Woof!");
+            GermanShepherd hans = new GermanShepherd("Hans", 35, true, true, 42);
+
+            animal.Speak();
+            
         }
 
 
@@ -168,6 +189,72 @@ namespace ClassBasics
             public long Add(long a, long b)
             {
                 return a + b;
+            }
+
+            public override string ToString()
+            {
+                return "Polymorphism Example Overriding method";
+            }
+        }
+
+
+        // Inheritance Example
+
+        // Base Class
+        public class Animal
+        {
+            public string Name {get;set;}
+
+            public string Speech {get;set;}
+
+            public Animal(string Name, string Speech){
+                this.Name = Name;
+                this.Speech = Speech;
+            }
+
+            public virtual void Speak(){
+                Console.WriteLine($"{Name} says {Speech}");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public int TeethCount {get;set;}
+            public bool IsDomesticated {get;set;}
+            public Dog(string Name, int TeethCount, bool IsDomesticated, string Speech) : base(Name, Speech){
+                this.TeethCount = TeethCount;
+                this.IsDomesticated = IsDomesticated;
+            }
+
+            public Dog(int TeethCount, bool IsDomesticated) : base("Lassie", "Timmy fell down the well!"){
+                this.TeethCount = TeethCount;
+                this.IsDomesticated = IsDomesticated;
+            }
+
+            public override void Speak()
+            {
+                // base.Speak();
+
+                Console.WriteLine($"Dog with name {Name} says {Speech}");
+            }
+
+        }
+
+        public class GermanShepherd : Dog
+        {
+
+            public bool HasFullTail = true;
+            public int DogShowWins = 0;
+
+            public GermanShepherd(string Name, int TeethCount, bool IsDomesticated, bool HasFullTail, int DogShowWins) : base(Name, TeethCount, IsDomesticated, "Komm her, Schafe")
+            {
+                this.HasFullTail = HasFullTail;
+                this.DogShowWins = DogShowWins;
+            }
+
+            public override void Speak()
+            {
+                Console.WriteLine($"German Shepherd called {Name} says {Speech}");
             }
         }
     }
