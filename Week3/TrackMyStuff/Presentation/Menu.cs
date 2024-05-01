6e7f63fdbbe074d824b210549d6b1e64222f20f2
jdelacruz96 <jonathan.delacruz@revature.com>
2024-05-01 11:40:51 -0400

    building the menu

--- Week3/TrackMyStuff/Presentation/Menu.cs
+++ Week3/TrackMyStuff/Presentation/Menu.cs
@@ -3,25 +3,41 @@ namespace TrackMyStuff.Presentation;
 public class Menu
 {
    
+   //This method displays the initial menu when the user runs the program
     public static void StartMenu() {
 
         int userChoice = 0;
+        bool validInput = true;
 
         Console.WriteLine("Welcome to TrackMyStuff!");
         Console.WriteLine("1. New user");
         Console.WriteLine("2. Returning user");
         Console.WriteLine("3. Exit program");
         
-        //Setting up the try-catch to handle user input
-        try
+        //Setting up the try-catch to handle user input with
+        //do-while to let them try again
+        do
         {
+            try
+            {
+                userChoice = Convert.ToInt32(Console.ReadLine());
+                validInput = true;
 
-        }
-        catch (Exception ex) 
-        {
-            Console.WriteLine(ex.StackTrace.ToString());
-        }
+                switch (userChoice)
+                {
+                    
+                }
 
+            }
+            catch (Exception ex) 
+            {   
+                validInput = false;
+                Console.WriteLine("Please enter a valid choice!");
+            }
+
+        } while (!validInput);
+
+        
     }
     
     
