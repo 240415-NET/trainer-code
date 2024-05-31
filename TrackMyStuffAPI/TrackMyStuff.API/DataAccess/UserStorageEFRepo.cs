054d4c7688cc926ece47e934baba05541cf7903a
jdelacruz96 <jonathan.delacruz@revature.com>
2024-05-31 12:37:13 -0400

    added data access EF stuff

--- TrackMyStuffAPI/TrackMyStuff.API/DataAccess/UserStorageEFRepo.cs
+++ TrackMyStuffAPI/TrackMyStuff.API/DataAccess/UserStorageEFRepo.cs
@@ -6,6 +6,8 @@ namespace TrackMyStuff.API.Data;
 
 public class UserStorageEFRepo : IUserStorageEFRepo
 {
+    //This holds our context object that will actually do the databsae data transfer stuff
+    //for us using prebuild EF Core methods
     private readonly TrackMyStuffContext _context;
     public UserStorageEFRepo(TrackMyStuffContext contextFromBuilder)
     {
@@ -15,6 +17,16 @@ public class UserStorageEFRepo : IUserStorageEFRepo
     public async Task<User?> CreateUserInDBAsync(User newUserSentFromUserService)
     {
         //Here we will actually create the user in our DB
+
+        //First we use the context object, reference the table we want to add the object to,
+        //and then use .Add() to add it to stage it for saving.
+        _context.Users.Add(newUserSentFromUserService);
+
+        //We can add as many objects as we need to before we save, but be mindful that nothing
+        //actually reflects in the DB until we call SaveChangesAsync().
+        
+        //This line, that we await, is what actually sticks our user into our database.
+        await _context.SaveChangesAsync();
 
         return newUserSentFromUserService;
     }
