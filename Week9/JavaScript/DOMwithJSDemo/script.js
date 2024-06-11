18991f4c0626a89118432ec09d023d153a4c87de
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-11 10:27:51 -0400

    building our js script file

--- Week9/JavaScript/DOMwithJSDemo/script.js
+++ Week9/JavaScript/DOMwithJSDemo/script.js
@@ -1,3 +1,17 @@
 //This will be our javascript file
+//It is attached to our HTML file via a script tag at the bottom of the body element.
 
-console.log("My script has also loaded in!");
+//So, first we have to wait for the DOM to fully finish loading. 
+//Inside of our javascript file, we can reference all the HTML (and the DOM structure) of
+//our index.html, using the "document" object. We don't have to instantiate it or create it or anything,
+//we can just start using it
+
+//We reference our document object, then use dot notation to run a function to add an event listener.
+//The event we are going to listen for, is 'DOMContentLoaded'.
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+
+
+
+
+}); //Closing the addEventListener method
