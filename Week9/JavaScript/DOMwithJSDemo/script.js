0943b045f4d920fb100b525d5b92ab5f21fd9787
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-11 10:38:21 -0400

    finishing up simple DOM manipulation tutorial

--- Week9/JavaScript/DOMwithJSDemo/script.js
+++ Week9/JavaScript/DOMwithJSDemo/script.js
@@ -8,7 +8,7 @@
 
 //We reference our document object, then use dot notation to run a function to add an event listener.
 //The event we are going to listen for, is 'DOMContentLoaded'.
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     //All of our DOM manipulation will be done within this lamba within addEventListener
 
     //So now, we will start to work with our HTML elements as Javascript variables/objects
@@ -18,7 +18,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //Now we will add a listener to that button we selected and stored in changeTextButton above
     changeTextButton.addEventListener('click', () => {
 
-        
+        console.log('Yep, my button was indeed clicked, and the HTML should be updated!');
+
+        //To update the text inside of out p-tag, we will select it via it's ID, as we did for the button above
+        //the name of our Javascript variable does not need to match the name of the ID in the html element we select
+        const paragraph = document.getElementById('textToChange');
+
+        paragraph.textContent = 'This is my new text that is coming in from my Javascript code!';
 
     });//Closing the button.addEventListener() method
 
