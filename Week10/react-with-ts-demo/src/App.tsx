5958368352fa08c2896c66c86b8e6fb716e6fbd8
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-17 10:02:58 -0400

    created my react demo app

--- Week10/react-with-ts-demo/src/App.tsx
+++ Week10/react-with-ts-demo/src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//This is our root component called App. We will create our own components that we then nest inside of or call 
+//from App later on. 
+//Notice that App is a FUNCTIONAL component. Its a TS function that returns HTML (with some slight syntax differences)
+//that is then rendered in the browser. 
 function App() {
   return (
     <div className="App">
