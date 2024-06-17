944975bbcc8d565a49573388cb8dd7eb06d01ec7
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-17 11:45:54 -0400

    created our cursed counter (class based)

--- Week10/react-with-ts-demo/src/components/ClassBasedCounter.tsx
+++ Week10/react-with-ts-demo/src/components/ClassBasedCounter.tsx
@@ -26,11 +26,37 @@ class ClassBasedCounter extends Component<{}, CounterClassState> {
         //Notice we lose access to things like setState(), and have to do this fairly manually
         this.state = { count: 0};
     }
+    //So our increment function looks a little different
+    //We need to reference our state object directly in order to update it
+    //Here, we reach in and grab the current value of count, and then replace 
+    //the count in our state object will count + 1 
+    increment = () => this.setState({count: this.state.count + 1 })
 
-    
+    decrement = () => this.setState({count: this.state.count - 1 })
 
-
-
+    //Because we are in a class, a class cannot simply "return" anything
+    //We will create a method called render() that then contains the returned HTML
+    render() {
+        return (
+            <div>
+            <h3>This is my class based counting-component</h3>
+            {/* This is a comment inside of my TSX file's HTML 
+            This will not render.
+            Notice below, we have a slight change to our break tag with the ending forward slash
+            HTML written inside of a react component does not like self closing tags. There are some other
+            slight syntax differences that we will encounter as we use react*/}
+            <br /> 
+            {/* Inside of return for my component, it expects HTML. We can "break out" of our HTML and call upon 
+            TypeScript by using these braces {}
+            
+            In my class based component, count is being stored in this.state.count
+            We need to reach into the state to read it*/}
+            <p>Count: {this.state.count}</p>
+            <button onClick={this.increment}>Increment the class based count!</button>
+            <button onClick={this.decrement}>Decrement the class based count :c </button>
+        </div>
+        );
+    }
 
 
 }
