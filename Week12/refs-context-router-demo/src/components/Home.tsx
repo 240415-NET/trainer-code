6846bca41856df420033d42f0d17d9a91986d124
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-24 10:22:40 -0400

    created my context and it's provider, using it along with refs in home

--- Week12/refs-context-router-demo/src/components/Home.tsx
+++ Week12/refs-context-router-demo/src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+//To use 
 
 function Home() {
   return (
--- Week12/refs-context-router-demo/src/context/MessageContext.tsx
+++ Week12/refs-context-router-demo/src/context/MessageContext.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createContext, useState, ReactNode } from 'react';
 
 
-
 //Defining an interface for our context value
 interface MessageContextType {
     message: string; //the message that will be shared/displayed around our app
@@ -13,15 +12,23 @@ interface MessageContextType {
 //In order to use context, we create and export 2 things: The context, and it's contextProvider
 export const MessageContext = createContext<MessageContextType | undefined>(undefined);
 
+//Here we will define props for our context provider 
+//We are going to use the ReactNode type (comes in with React) to simplify this
+interface MessageContextProviderProps {
+    children: ReactNode;
+}
 
+function MessageContextProvider( {children}: MessageContextProviderProps ) {
 
-
-function MessageContextProvider() {
-
+    //Inside of our MessageContextProvider functional component we will save state information
+    const [message, setMessage] = useState<string>('Hello from my MessageContext.tsx!');
 
   return (
-    <div>MessageContext</div>
+    <MessageContext.Provider value={{ message , setMessage }}>
+        {children}
+    </MessageContext.Provider>
   )
 }
 
+//Exporting our message context provider
 export default MessageContextProvider;
