71f23e1af28e8fc33dd0fd7a61ebc35d006d3612
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-24 10:28:03 -0400

    adding Home HTML to use later

--- Week12/refs-context-router-demo/src/components/Home.tsx
+++ Week12/refs-context-router-demo/src/components/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-//To use 
+//To use refs and context we have to import them
+import React, { useContext , useRef } from 'react'
+
+//I also have to import my context (hint: not the provider! Just the context.)
+import { MessageContext } from '../context/MessageContext'
 
 function Home() {
+
+
+
+
+
+
   return (
-    <div>Home works!</div>
+    <div>
+      <h2>Home</h2>
+      {/* In my p tag below, I will display the message from context */}
+      <p></p>
+      {/* Input field that will update my ref */}
+      <input type="text" />
+      {/* Button that will update my message */}
+      <button onClick={}>Update Message</button>
+    </div>
   )
 }
 
