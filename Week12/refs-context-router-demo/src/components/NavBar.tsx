bb15a765b15feb9f12898c18d9abd35d11ed1d46
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-24 09:37:17 -0400

    finished routing

--- Week12/refs-context-router-demo/src/components/NavBar.tsx
+++ Week12/refs-context-router-demo/src/components/NavBar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar works!</div>
+    <nav>
+        <ul>
+            {/* Link to my home component */}
+            <li><Link to="/">Home</Link></li> 
+
+            {/* Link to my about component */}
+            <li><Link to="/about">About</Link></li>
+            
+            {/* Link to my dashboard component */}
+            <li><Link to="/dashboard">Dashboard</Link></li>
+        </ul>
+    </nav>
   )
 }
 
