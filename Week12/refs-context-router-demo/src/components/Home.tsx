7155bf7f07684407d906cb0ac77ebe3c8ed048f8
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-24 10:35:40 -0400

    created the ref and called context in Home.tsx

--- Week12/refs-context-router-demo/src/components/Home.tsx
+++ Week12/refs-context-router-demo/src/components/Home.tsx
@@ -6,10 +6,20 @@ import { MessageContext } from '../context/MessageContext'
 
 function Home() {
 
+  //Accessing context (MessageContext in this case) with the useContext hook
+  //The exclamation point at the end before my semicolon tells me that context may be null or undefined
+  //If I just opened my application, there may be nothing there at all!
+  const {message, setMessage} = useContext(MessageContext)!;
 
+  //Creating a ref to hold the input element reference
+  const inputRef = useRef<HTMLInputElement>(null);
 
-
-
+  //Function to update the context message using the input value
+  function updateContextMessage() {
+    if (inputRef.current) { //If there is something currently in out inputRef
+      setMessage(inputRef.current.value);//... give it the current value of inputRef
+    }
+  }
 
   return (
     <div>
@@ -17,9 +27,9 @@ function Home() {
       {/* In my p tag below, I will display the message from context */}
       <p></p>
       {/* Input field that will update my ref */}
-      <input type="text" />
+      <input type="text" ref={inputRef}/>
       {/* Button that will update my message */}
-      <button onClick={}>Update Message</button>
+      <button onClick={updateContextMessage}>Update Message</button>
     </div>
   )
 }
