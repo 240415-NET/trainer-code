dca026e6cb6f65ee4a2df0f6747fa7f714bcd7d1
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-24 09:21:27 -0400

    added my imports to app.tsx, starting routing

--- Week12/refs-context-router-demo/src/App.tsx
+++ Week12/refs-context-router-demo/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+//Im going to import my router, that I downloaded with that npm install
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+
+    </BrowserRouter>
   );
 }
 
