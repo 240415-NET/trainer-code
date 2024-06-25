824ed6be854b25604fba7df2a13b7bf9884e25aa
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-25 11:47:25 -0400

    selected out button and text input

--- Week12/refs-context-router-demo/src/components/Home/Home.test.tsx
+++ Week12/refs-context-router-demo/src/components/Home/Home.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './Home';
 //Importing fireEvent, that is going to let us simulate users cliking/interacting with our component
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent, queryByAttribute } from '@testing-library/react';
 //Importing our message context provider, so we can render our component with the Message context 
 //available/given to it
 import MessageContextProvider from '../../context/MessageContext';
@@ -17,5 +17,23 @@ test('Renders Home component, including context message', () => {
         </MessageContextProvider>
     );
 
-    expect(screen.getByText('Hello from Context!')).toBeInTheDocument();
+    expect(screen.getByText('Hello from my MessageContext.tsx!')).toBeInTheDocument();
+});
+
+//This test will use fireEvent() to simulate a user clicking on our button
+//We will also provide some new simulated user input in our text input field
+test('Updates context message on button click', () => {
+    //Again, we will render our component wrapped by the MessageContextProvider
+    render(
+        <MessageContextProvider>
+            <Home />
+        </MessageContextProvider>
+    );
+
+    //Now we will select our input field and out button so that we can work with them using fireEvent()
+    const messageUpdateInput = screen.getByRole('textbox'); //Selecting our text box
+    const messageUpdateButton = screen.getByRole('button', {name: /update message/}); //select our button
+
+
+
 });
