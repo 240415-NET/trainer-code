f0fc97b02cb4a565a7f02543a18406ad04b4c735
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-25 10:34:15 -0400

    added about.test.tsx

---
+++ Week12/refs-context-router-demo/src/components/About/About.test.tsx
@@ -0,0 +1,7 @@
+//This test file, will test our About component
+//It will make sure that we are rendering it properly.
+import React from 'react';
+//We import the component that is under test in this file
+import About from './About';
+//We import what we need from the react testing library we just npm installed
+import { render, screen } from '@testing-library/react'
