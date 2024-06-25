b7a7242667cb4dbaa184ace5c1bb7e73cdc9d457
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-25 11:35:05 -0400

    wrote our Home rendering test

--- Week12/refs-context-router-demo/src/components/Home/Home.test.tsx
+++ Week12/refs-context-router-demo/src/components/Home/Home.test.tsx
@@ -5,3 +5,17 @@ import { render, screen, fireEvent} from '@testing-library/react';
 //Importing our message context provider, so we can render our component with the Message context 
 //available/given to it
 import MessageContextProvider from '../../context/MessageContext';
+
+
+//Our first test, will ensure our component is rendering properly.
+//Including the message that comes in via the context
+test('Renders Home component, including context message', () => {
+    //Rendering our home component, wrapping it in the MessageContextProvider
+    render(
+        <MessageContextProvider>
+            <Home />
+        </MessageContextProvider>
+    );
+
+    expect(screen.getByText('Hello from Context!')).toBeInTheDocument();
+});
