ca817b86f46f072a37e8658f1fe1bf397a019001
jdelacruz96 <jonathan.delacruz@revature.com>
2024-06-25 10:38:25 -0400

    wrote our first Jest test!

--- Week12/refs-context-router-demo/src/components/About/About.test.tsx
+++ Week12/refs-context-router-demo/src/components/About/About.test.tsx
@@ -5,3 +5,11 @@ import React from 'react';
 import About from './About';
 //We import what we need from the react testing library we just npm installed
 import { render, screen } from '@testing-library/react'
+
+test('Correctly renders About component' , () => {
+    //We are going to use "render" to render out component, referncing it with it's react 
+    //HTML element tag
+    render(<About/>);
+    expect(screen.getByText('About')).toBeInTheDocument();
+    expect(screen.getByText("This is the about page.")).toBeInTheDocument();
+});
